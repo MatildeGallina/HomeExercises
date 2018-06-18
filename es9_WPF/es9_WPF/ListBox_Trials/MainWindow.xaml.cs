b1bc0b3d0ff153de30b8811740f75e66c5ba41fa
matilde gallina <matilde.gallina@outlook.com>
2018-06-19 00:22:58 +0200

    Primi trials con Wpf Grid e ListBox

---
+++ es9_WPF/es9_WPF/ListBox_Trials/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ListBox_Trials
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<Riserva> list = new List<Riserva>();
+
+            list.Add(new Riserva() { Type = "Gasolio", Percentage = 80});
+            list.Add(new Riserva() { Type = "Liquido refrigerante", Percentage = 20 });
+            list.Add(new Riserva() { Type = "Olio", Percentage = 50 });
+
+            ListRiserve.ItemsSource = list;
+        }
+
+        private void Pieno_Click(object sender, RoutedEventArgs e)
+        {
+            List<Riserva> list = new List<Riserva>();
+
+            foreach (Riserva r in ListRiserve.SelectedItems)
+                if (r.Percentage < 100)
+                {
+                    r.Percentage = 100;
+                    list.Add(r);
+                }
+
+            ListRiserve.ItemsSource = list;
+        }
+
+        private void E20_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Riserva r in ListRiserve.SelectedItems)
+                if (r.Percentage < 100)
+                    r.Percentage += 10;
+        }
+
+        private void E10_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Riserva r in ListRiserve.SelectedItems)
+                if (r.Percentage < 100)
+                    r.Percentage = 20;
+        }
+    }
+
+    public class Riserva
+    {
+        public string Type { get; set; }
+        public int Percentage { get; set; }
+    }
+}
