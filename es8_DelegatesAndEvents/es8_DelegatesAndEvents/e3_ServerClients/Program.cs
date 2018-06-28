de8ed2f03c391e3c7719135987bd266a67dc4c07
matilde gallina <matilde.gallina@outlook.com>
2018-06-28 22:35:41 +0200

    Start ex ClientServer

---
+++ es8_DelegatesAndEvents/es8_DelegatesAndEvents/e3_ServerClients/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace e3_ServerClients
+{
+    class Program
+    {
+        /* Una classe `Server` ha un evento per inviare messaggi.
+         * Una classe `Client` ha uno o più metodi per gestire tali messaggi.
+         * Generare le due classi, il delegate dell'evento, l'evento sul `Server`, e i metodi sul `Client`.
+         * Provare nel `Main()` ad agganciare e sganciare alcuni `Client` dal `Server`, e verificare il corretto funzionamento.
+         */
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Server
+    {
+        public event Func<string, string> NewMessage;
+    }
+}
